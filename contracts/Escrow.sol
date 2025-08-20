215f9c6493d37226c3a0cacc3d643eb192b3cc2a
Habib Adavize Abdulkareem <habibadavize94@gmail.com>
2025-08-20 01:06:24 +0100

    Update Started On the Escrol sol and it test

--- contracts/Escrow.sol
+++ contracts/Escrow.sol
@@ -2,13 +2,29 @@
 pragma solidity ^0.8.0;
 
 interface IERC721 {
-    function transferFrom(
-        address _from,
-        address _to,
-        uint256 _id
-    ) external;
+    function transferFrom(address _from, address _to, uint256 _id) external;
 }
 
 contract Escrow {
+    address public lender;
+    address public inspector;
+    address public buyer;
+    address payable public seller;
+    address public nftAddress;
 
+    constructor(
+        address _lender,
+        address _inspector,
+        address _buyer,
+        address payable _seller,
+        address _nftAddress
+    ) {
+        lender = _lender;
+        inspector = _inspector;
+        buyer = _buyer;
+        seller = _seller;
+        nftAddress = _nftAddress;
+    }
+
+    
 }
--- test/Escrow.js
+++ test/Escrow.js
@@ -6,5 +6,32 @@ const tokens = (n) => {
 }
 
 describe('Escrow', () => {
+    let buyer, seller, inspector, lender
 
+    let realEstate, escrow
+
+    it('saves the addresses', async () => {
+
+        //Setup accounts
+        [buyer, seller, inspector, lender] = await ethers.getSigners();
+
+        //Deploy the RealEstate contract
+        const RealEstate = await ethers.getContractFactory('RealEstate');
+        realEstate = await RealEstate.deploy();
+        //await realEstate.deployed();
+
+        //Mint
+        let transaction = await realEstate.connect(seller).mint('https://ipfs.io/ipfs/QmTudSYeM7mz3PkYEWXWqPjomRPHogcMFSq7XAvsvsgAPS');
+
+        await transaction.wait()
+
+        const Escrow = await ethers.getContractFactory('Escrow')
+        escrow = await Escrow.deploy(
+            realEstate.address,
+            seller.address,
+            inspector.address,
+            lender.address
+
+        )
+    })
 })
